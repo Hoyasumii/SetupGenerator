72e8e096ef7256881fdc68e829f4dfc9517f47e3
Alan Reis <alanreisanjo@gmail.com>
2024-01-03 04:24:20 -0300

    Mais correções no gerador de setup

--- SetupGenerator/setupGenerator.py
+++ SetupGenerator/setupGenerator.py
@@ -38,17 +38,26 @@ def setupGenerator():
     packages = find_packages()
 
     # Get Virtual Enviroment name
-    venv = os.environ.get("VIRTUAL_ENV").split("\\")[-1]
+    venv = os.environ.get("VIRTUAL_ENV")
 
-    install_requires = None
+    if venv is not None:
+        venv = venv.split("\\")[-1]
+
+        install_requires = None
+        
+        if os.name == "nt":
+            install_requires = subprocess.run([f".\\{ venv }\\Scripts\\python", "-m", "pip", "freeze"], capture_output=True)
+        else:
+            install_requires = subprocess.run(["source", f"./{ venv }/bin/python", "-m", "pip", "freeze"], capture_output=True)
+
+        install_requires = install_requires.stdout.decode("utf-8").split("\n")
+        install_requires = [ package[:-1] for package in install_requires if package != "" ]
     
-    if os.name == "nt":
-        install_requires = subprocess.run([f".\\{ venv }\\Scripts\\python", "-m", "pip", "freeze"], capture_output=True)
     else:
-        install_requires = subprocess.run(["source", f"./{ venv }/bin/python", "-m", "pip", "freeze"], capture_output=True)
-
-    install_requires = install_requires.stdout.decode("utf-8").split("\n")
-    install_requires = [ package[:-1] for package in install_requires if package != "" ]
+        if os.name == "nt":
+            install_requires = subprocess.run(["python", "-m", "pip", "freeze"], capture_output=True)
+        else:
+            install_requires = subprocess.run(["python3", "-m", "pip", "freeze"], capture_output=True)
 
     entry_points = { 'console_scripts': [ ] }
     

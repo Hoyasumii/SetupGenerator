f9b3f8433512897e1c96ef50274b320474acb6d4
Alan Reis <alanreisanjo@gmail.com>
2024-04-03 02:24:59 -0300

    Adição de porte para Linux

--- SetupGenerator/__init__.py
+++
@@ -1,1 +0,0 @@
-from .setupGenerator import setupGenerator
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if os.path.isfile("README.md"):
 
 setup(
     name="setup-generator",
-    version=1.0,
+    version=1.1,
     description="Pequena biblioteca para criar um setup.py rápido.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/Hoyasumii/SetupGenerator",
     packages=['setupGenerator', 'setupGenerator.scripts'],
     package_data={'setupGenerator': ['.template']},
-    install_requires=['Jinja2==3.1.2', 'keyboard==0.13.5', 'MarkupSafe==2.1.3', 'python-slugify==8.0.1', 'setuptools==69.0.3', 'simple-form==1.0', 'text-unidecode==1.3'],
+    install_requires=['Jinja2==3.1.2', 'keyboard==0.13.5', 'MarkupSafe==2.1.3', 'python-slugify==8.0.1', 'setuptools==69.0.3', 'simple-form==1.4', 'text-unidecode==1.3'],
     license="GPL-3.0",
     entry_points={'console_scripts': [
         "setup-generator = setupGenerator.setupGenerator:setupGenerator"
